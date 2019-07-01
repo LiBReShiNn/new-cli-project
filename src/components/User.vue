4e81dab4dad51037b3c7829de7bf335c4ab9855a
HJ-ShiNn <hj.shin@infinigru.com>
2019-07-01 21:08:42 +0900

    중괄호 위치 조심. Props{}, computed{}, methods{}

--- src/components/User.vue
+++ src/components/User.vue
@@ -6,9 +6,7 @@
         <hr>
         <v-layout row wrap>
             <v-flex xs12 sm6>
-<!--                <UserDetail v-bind:nameOfChild="name"></UserDetail>-->
-                <UserDetail :nameOfChild="{name : 'LiBRe', familyName: 'ShiNn'}"></UserDetail>
-<!--                nameOfChild라는 배열이름으로 name, familyName자료를 넘겨줌-->
+                <UserDetail :nameOfChild="name"></UserDetail>
             </v-flex>
             <v-flex xs12 sm6>
                 <UserEdit></UserEdit>
@@ -20,20 +18,19 @@
 <script>
     import UserDetail from './UserDetail.vue';
     import UserEdit from './UserEdit.vue';
-    // 1. 홈 컴포넌트를 가져는 왓는데 이것을 사용하기 위해서는 이것이 컴포넌트라고 선언을 해주어야한다.
     export default {
         components: {
-            UserDetail, //2.홈 컴포넌트를 컴포넌트라고 선언
+            UserDetail,
             UserEdit
         },
         data() {
             return {
-                name: 'LiBReShiNn' //세미콜론 찍으면 오류남. 데이터 전송도 중에 끊어버리는 것과 같은 것
+                name: 'LiBReShiNn'
             }
         },
         methods: {
             changeName() {
-                this.name = "ShiNni"
+                this.name = 'ShiNni'
             }
         }
     }
--- src/components/UserDetail.vue
+++ src/components/UserDetail.vue
@@ -1,34 +1,28 @@
 <template>
     <div class="red lighten-3 pa-3">
         <h3>자세한 회원 정보를 확인합니다.</h3>
-        <!--        <p>이름 : {{ name }}</p>-->
-        <p>{{ nameOfChild.name }}</p>
-        <p>{{ nameOfChild.familyName }}</p>
+        <p>{{ nameOfChild }}</p>
         <p>{{ sayHello }}</p>
+        <v-btn @click="switchName()">이름변경</v-btn>
     </div>
 </template>
 
 <script>
     export default {
-        // 부모컴포넌트에서 선언한 변수명으로 배열받음
         props: {
-            // 값은 이미 부모에게서 받았으므로 값을 기록할 수 없고 데이터 타입만 기록이 가능하다
-            // nameOfChild:Object
-            // nameOfChild:String
-            // nameOfChild를 객체로 선언 가능
             nameOfChild: {
-                // type: String, //(자료형 에러)
-                type: Object,
-                required: true,//(전달 받았는지 확인가능, default와 함께 쓰면 충돌)
-                default: 'LEGO'//(값을 전달 받지 못했을 경우 default값, 값을 전달 받았을 경우 해당 값)
-
+                type: String, //(자료형 에러)
+                default: 'LEGO', //(값을 전달 받지 못했을 경우 default값
             },
-            computed: {
-                sayHello() {
-                    // return this.nameOfChild.name + ' 안녕하세요'; (X)
-                    // 선언시 객체가 존재하지 않기 때문?
-                    return this.nameOfChild.name + ' 안녕하세요';
-                }
+        },
+        computed: {
+            sayHello() {
+                return this.nameOfChild + ' 안녕하세요'
+            }
+        },
+        methods: {
+            switchName() {
+                this.nameOfChild = '컴퓨터'
             }
         }
     }
